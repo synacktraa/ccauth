f82ac20d7f9865ca432dc612712b84a193a9ca68
synacktra.work@gmail.com <synacktra.work@gmail.com>
2026-05-14 14:41:42 +0530

    feat: expose custom code capturer runner

--- ccauth/__init__.py
+++ ccauth/__init__.py
@@ -1,4 +1,4 @@
 from .errors import AuthError, CCAuthError, ModeError
-from .runner import run_auth
+from .runner import run_auth, run_auth_custom
 
-__all__ = ["AuthError", "CCAuthError", "ModeError", "run_auth"]
+__all__ = ["AuthError", "CCAuthError", "ModeError", "run_auth", "run_auth_custom"]
